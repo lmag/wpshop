5b9ad7f3ab4286991694981771b3e3ab8fa9f338
evarisk-kilyan <kilyan.evarisk@gmail.com>
2025-07-16 17:15:05 +0200

    #246 [AccountDetailsBlock] fix: country name is not a number

--- blocks/build/my-account-block/inner-blocks/account-details/render.php
+++ blocks/build/my-account-block/inner-blocks/account-details/render.php
@@ -1,2 +1,2 @@
-<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> data-countries="<?php echo esc_attr( json_encode( \wpshop\get_countries() ) ); ?>">
 </div>
